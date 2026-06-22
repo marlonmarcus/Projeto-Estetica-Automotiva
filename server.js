a44fa59a6c9d1518ab62091fed0f0e24e144f435
Marlon Marcus <marcusmarlon25@gmail.com>
2026-06-22 19:58:11 -0300

    fazendo o formulário do cliente e do servidor funcionar

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-const express = require ("express");
+const express = require("express");
 
-const path = require ("path");
+const path = require("path");
+
 const app = express();
+app.use(express.urlencoded ({ extended: true}));
+
+//FAZENDO APARECER O CSS
 app.use(express.static("public"));
+
+// FAZENDO COM QUE O CLIENTE RECEBA O HTML
 app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname,"public","index.html"));
 });
+
+// FAZENDO AGENDAMENTO
+app.post("/agendar", (req, res)=>{
+    console.log(req.body);
+
+    res.send("Agendamento recebido com sucesso!");
+});
+
+//CONFIRMAÇÃO DO SERVIDOR RODANDO
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000");
 });
